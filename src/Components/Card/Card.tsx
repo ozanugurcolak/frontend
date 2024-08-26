30205fe31f14c0639f87fa303134cb5f80dced58
ozanugurcolakk <ozanugurcolak@gmail.com>
2024-08-26 17:48:24 +0300

    Add and delete portfolio

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import "./Card"
 import { CompanySearch } from '../../company';
+import AddPortfolio from '../Portfolio/AddPortfolio';
 interface Props  {
     id : string;
     searchResult: CompanySearch;
-
+    onPortfolioCreate: (e:SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({id,searchResult}: Props) : JSX.Element => {
+const Card: React.FC<Props> = ({id,searchResult,onPortfolioCreate}: Props) : JSX.Element => {
   return (
     <div className='card'>
         <img alt="CompanyLogo" />
@@ -16,6 +17,7 @@ const Card: React.FC<Props> = ({id,searchResult}: Props) : JSX.Element => {
             <p>{searchResult.currency}</p>
         </div>
         <p className='info'>{searchResult.exchangeShortName} - {searchResult.stockExchange} </p>
+        <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResult.symbol}/>
     </div>
     
 
---
+++ src/Components/Portfolio/CardPortfolio.tsx
@@ -0,0 +1,22 @@
+import React, { SyntheticEvent } from "react";
+import DeletePortfolio from "./DeletePortfolio";
+
+interface Props {
+    portfolioValue: string
+    onPortfolioDelete: (e: SyntheticEvent) => void;
+
+}
+
+const CardPortfolio = ({ portfolioValue, onPortfolioDelete }: Props) => {
+    return (
+        <>
+            <h4>{portfolioValue}</h4>
+            <DeletePortfolio
+                portfolioValue={portfolioValue}
+                onPortfolioDelete={onPortfolioDelete}
+            />
+        </>
+    )
+}
+
+export default CardPortfolio
