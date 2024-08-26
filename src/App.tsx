f5560bc4a33406517783d6c1d5a50d85f372d48a
ozanugurcolakk <ozanugurcolak@gmail.com>
2024-08-26 17:48:24 +0300

    tailwindcss added

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,58 @@ import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
 import { CompanySearch } from './company';
 import { searchCompanies } from './api';
+import ListPortfolio from './Components/Portfolio/ListPortfolio';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [portfolioValues,setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
+  const onPortfolioCreate = (e:any) => {
+    e.preventDefault()
+    const exists = portfolioValues.find((value) => value ===e.target[0].value)
+    if(exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value]
+    setPortfolioValues(updatedPortfolio) 
+  }
 
-  const onClick = async (e: SyntheticEvent) => {
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const removed = portfolioValues.filter((value) => {
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(removed);
+  };
+
+
+  const onSearchSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
     const result = await searchCompanies(search);
     if (typeof result === "string") {
       setServerError(result);
     } else if (Array.isArray(result)) {
-      setSearchResult(result); // Search results are set here
+      setSearchResult(result); 
     }
   };
 
   useEffect(() => {
-    console.log(searchResult); // This will log the updated search results
-  }, [searchResult]); // Triggered whenever searchResult is updated
+    console.log(searchResult); 
+  }, [searchResult]); 
 
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange} />
+      <Navbar/>
+       
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
       {serverError ? <div>Error: {serverError}</div> : null}
-      <CardList searchResults={searchResult} />
+      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />   
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
     </div>
   );
 }
--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import Card from '../Card/Card'
 import { CompanySearch } from '../../company'
 import { v4 as uuidv4 } from 'uuid'
 
-interface Props {searchResults: CompanySearch[]}
+interface Props {
+  searchResults: CompanySearch[]
+  onPortfolioCreate: (e: SyntheticEvent) => void;
+}
 
-const CardList : React.FC<Props> = ({searchResults}) : JSX.Element => {
+const CardList: React.FC<Props> = ({ searchResults, onPortfolioCreate }): JSX.Element => {
   return (
-    <>
-        {searchResults.length > 0 ? (
-          searchResults.map((result)=>{
-            return <Card id={result.symbol} key={uuidv4()} searchResult={result} />
-          })
-        ): (
-          <h1>No results</h1>
-        )}
+    <div>
+      {searchResults.length > 0 ? (
+        searchResults.map((result) => {
+          return <Card id={result.symbol} key={uuidv4()} searchResult={result} onPortfolioCreate={onPortfolioCreate} />
+        })
+      ) : (
+        <p className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+          No results!
+        </p>
+      )}
 
-    </>
+    </div>
   )
 }
 
