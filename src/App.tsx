2e3c150b398f29b30bff02a2c80cc1afc595c41c
ozanugurcolakk <ozanugurcolak@gmail.com>
2024-08-27 15:59:36 +0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,14 @@
-import { ChangeEvent, SyntheticEvent, useState, useEffect } from 'react';
+import { Outlet } from 'react-router';
 import './App.css';
-import CardList from './Components/CardList/CardList';
-import Search from './Components/Search/Search';
-import { CompanySearch } from './company';
-import { searchCompanies } from './api';
-import ListPortfolio from './Components/Portfolio/ListPortfolio';
 import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [portfolioValues,setPortfolioValues] = useState<string[]>([]);
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string | null>(null);
-
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-  const onPortfolioCreate = (e:any) => {
-    e.preventDefault()
-    const exists = portfolioValues.find((value) => value ===e.target[0].value)
-    if(exists) return;
-    const updatedPortfolio = [...portfolioValues, e.target[0].value]
-    setPortfolioValues(updatedPortfolio) 
-  }
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    const removed = portfolioValues.filter((value) => {
-      return value !== e.target[0].value;
-    });
-    setPortfolioValues(removed);
-  };
-
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(search);
-    if (typeof result === "string") {
-      setServerError(result);
-    } else if (Array.isArray(result)) {
-      setSearchResult(result); 
-    }
-  };
-
-  useEffect(() => {
-    console.log(searchResult); 
-  }, [searchResult]); 
 
   return (
-    <div className="App">
-      <Navbar/>
-       
-      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
-      {serverError ? <div>Error: {serverError}</div> : null}
-      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />   
-      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
-    </div>
+    <>
+      <Navbar />
+      <Outlet/>
+    </>
   );
 }
 
---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import HomePage from "../Pages/HomePage";
+import SearchPage from "../Pages/SearchPage";
+import CompanyPage from "../Pages/CompanyPage";
+
+export const router = createBrowserRouter([{
+    path:  "/",
+    element: <App/>,
+    children: [
+        {path: "", element: <HomePage/>},
+        {path: "search", element: <SearchPage/>},
+        {path: "company/:ticker", element: <CompanyPage/>}
+    ]
+}])
