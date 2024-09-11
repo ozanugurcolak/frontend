a879abb330333d9096a696d46aa5e171a79c6aea
ozanugurcolakk <ozanugurcolak@gmail.com>
2024-09-11 11:20:27 +0300

    AuthService

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Outlet/>
+      <ToastContainer/>
     </>
   );
 }
---
+++ src/Components/Helpers/ErrorHandler.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const handleError = (error: any) => {
+  if (error.response) {
+    const err = error.response;
+    
+    if (Array.isArray(err.data.errors)) {
+      for (let val of err.data.errors) {
+        toast.warning(val.description);
+      }
+    } else if (typeof err.data.errors === "object") {
+      for (let e in err.data.errors) {
+        toast.warning(err.data.errors[e][0]);
+      }
+    } else if (err.data) {
+      toast.warning(err.data);
+    } else if (err.status === 401) {
+      toast.warning("Please login");
+      window.history.pushState({}, "LoginPage", "/login");
+    } else {
+      toast.warning("An unexpected error occurred.");
+    }
+  } else {
+    toast.error("An unexpected error occurred.");
+    console.error(error);
+  }
+};
